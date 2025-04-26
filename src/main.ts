59385d45d44233f6b4ba8ba6f4c337ec431290de
Griffith000 <latrechghassen68@example.com>
2025-04-26 01:09:39 +0100

    revert

--- src/main.ts
+++ src/main.ts
@@ -9,11 +9,7 @@ import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ['https://micadmin.netlify.app'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    credentials: true,
-  });
+  app.enableCors();
 
   initSecurityConfig(app);
 
